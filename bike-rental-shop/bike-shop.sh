eed75c74c6cdb5627f604694b70e9038aaa75618
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-01 17:53:25 +0000

    Added a main menu where users get to pick the service they desire.

--- bike-rental-shop/bike-shop.sh
+++ bike-rental-shop/bike-shop.sh
@@ -4,3 +4,37 @@ PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
+MAIN_MENU(){
+
+     if [[ $1 ]]
+     then
+          echo -e "\n$1"
+     fi
+
+     echo How may I help you?
+     echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+
+     read MAIN_MENU_SELECTION
+
+     case $MAIN_MENU_SELECTION in
+          1) RENT_MENU ;;
+          2) RETURN_MENU ;;
+          3) EXIT ;;
+          *) MAIN_MENU "Please enter a valid option." ;;
+     esac
+}
+
+RENT_MENU () {
+    echo Rent Menu
+}
+
+function RETURN_MENU {
+     echo Return Menu
+}
+
+EXIT () {
+     echo -e "\nThank you for stopping in.\n"
+}
+
+
+MAIN_MENU
